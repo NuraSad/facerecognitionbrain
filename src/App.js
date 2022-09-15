3c998cde58e67ce15d01616c161a4571ec483d99
Ainur Saduova <ainur.saduova10@gmail.com>
2022-09-15 12:28:00 -0400

    Connecting FaceDetec API

--- src/App.js
+++ src/App.js
@@ -5,20 +5,73 @@ import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particle from './components/Particle';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+    
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      IMAGE_URL: "",
+    }
+  }
 
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+  
+  onButtonSubmit = () => {
+    this.setState({IMAGE_URL : this.state.input});
+    const USER_ID = "lunakuro";
+    const PAT = "a759a1ef50f94e78b7c19c8d000dbd17";
+    const APP_ID = "my-first-application";
+    const MODEL_ID = "face-detection";
+    const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";  
 
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": this.state.input
+                  }
+              }
+          }
+      ]
+    });
 
-function App() {
-  return (
-    <div className="App">
-      <Particle />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-    </div>
-  );
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+        .then(response => response.json())
+        .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
+        .catch(error => console.log('error', error));
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <Particle />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition ImageUrl={this.state.IMAGE_URL}/>
+      </div>
+    );
+  }
 }
 
 export default App;
